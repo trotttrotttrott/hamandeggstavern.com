6e597697c9fde5fb48da3f62fe1a898201663359
trotttrotttrott <trott@odaacabeef.com>
2012-09-10 01:44:21 -0700

    config.assets.initialize_on_precompile=false

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,9 @@ module HamAndEggs
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # On config/application.rb forcing your application to not access the DB
+    # or load models when precompiling your assets.
+    config.assets.initialize_on_precompile = false
   end
 end
