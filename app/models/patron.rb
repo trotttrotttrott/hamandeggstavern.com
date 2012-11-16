d0f9b515e35f7aae2322ba561ee99e0f5a8753bb
trotttrotttrott <trott@odaacabeef.com>
2012-11-16 21:57:10 -0200

    social stuff, new font, favicon, copy updates

--- app/models/patron.rb
+++ app/models/patron.rb
@@ -1,6 +1,6 @@
 class Patron < ActiveRecord::Base
   validates :email,
-            :presence => { :message => %q(can't be blizank.) },
-            :format => { :with => Devise.email_regexp, :message => %q(can't be nonsense.) },
-            :uniqueness => { :message => %q(is already in the d-base. Thanks for your interest.) }
+            :presence => true,
+            :format => { :with => Devise.email_regexp },
+            :uniqueness => true
 end
