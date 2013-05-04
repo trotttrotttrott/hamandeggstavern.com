03cd89fdc093643aaaceb215db1260917a9c0acf
trotttrotttrott <trott@odaacabeef.com>
2013-05-03 18:58:22 -0700

    Default scope for acts & event_participations

--- app/models/act.rb
+++ app/models/act.rb
@@ -1,4 +1,5 @@
 class Act < ActiveRecord::Base
   attr_accessible :name, :url, :description
   has_many :event_participations, :dependent => :destroy
+  default_scope order('name asc')
 end
--- app/models/event_participation.rb
+++ app/models/event_participation.rb
@@ -2,5 +2,6 @@ class EventParticipation < ActiveRecord::Base
   attr_accessible :event_id, :act_id, :act_rank
   belongs_to :event
   belongs_to :act
+  default_scope joins(:event).order('events.start')
   scope :by_rank, order(:act_rank)
 end
